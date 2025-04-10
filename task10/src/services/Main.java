e734b12a35a16f70df032e75e8d2ef7d5fd6ce1f
andirahim <burcifiqi@gamil.com>
2025-04-10 18:23:40 +0300

    commit #2
    in the nv on line 6 Execute the processor with the API-based service , on line 7  Create an instance of FileDataService with a file path, on line 8 Inject the fileService into another DataProcessor instance, on line 9 Execute the processor with the File-based service

--- task10/src/services/Main.java
+++ task10/src/services/Main.java
@@ -3,3 +3,9 @@ public class Main {
     public static void main(String[] args) {
         DataService apiService = new ApiDataService("API-123-XYZ");
         DataProcessor processor1 = new DataProcessor(apiService);
+        processor1.execute();
+        DataService fileService = new FileDataService("/data/input.txt");
+        DataProcessor processor2 = new DataProcessor(fileService);
+        processor2.execute();
+    }
+}
